76afea582455d3bc113b7fc4498d0b5451e689e2
DulcePC <d.perez@kation.com.do>
2023-10-17 20:22:07 -0400

    init property detail

--- pages/search/[id].vue
+++ pages/search/[id].vue
@@ -1,16 +1,56 @@
 <template>
-  <div v-if="property">
-    <PopulationSearchDetailVehicleSlides
-      :plan-type="property.plan_id"
-      :images="property.auto.images"
-    />
-    <PopulationSearchDetailVehicleInformation
-      :property="property.auto"
-      :user="property.user"
-    />
-    <PopulationSearchDetailVehicleLoan :property="property.auto" class="md:px-14 px-4" />
-    <OrganismExploreVehicles :property_id='property.plan_id' />
-  </div>
+  <main v-if="!pending" class="lg:px-20 md:px-8 px-5">
+    <div class="flex w-full items-center justify-center">
+      <figure>
+        <img src="/img/carol1.png" alt="">
+      </figure>
+      <figure>
+        <img src="/img/carol2.png" alt="">
+      </figure>
+      <figure>
+        <img src="/img/carol3.png" alt="">
+      </figure>
+    </div>
+    <section class="max-w-[1250px] mx-auto flex flex-col lg:px-5">
+      <div class="relative flex lg:mb-[60px] mb-8 lg:flex-row flex-col pt-10 justify-between items-center">
+        <figure class="bg-neutral-white p-6 w-[170px] h-[170px] border-2 rounded-lg flex items-center justify-center border-[#F5F5F5] absolute lg:-top-2/3 -top-1/4 ">
+          <img src="/img/business.png" alt="">
+        </figure>
+        <h2 class="lg:ml-[200px] mt-[90px] lg:mt-0 xl:text-5xl text-4xl font-semibold text-primary-100 mr-4 whitespace-nowrap">{{ advertisement.business.name}}</h2>
+        <div class="flex gap-3.5 text-sm mt-4 lg:mt-0 flex-wrap md:justify-end justify-center">
+          <p class="hour" :class="{closed: !open}">
+            <AtomsIcon name="general/clock" :size=32 class="absolute left-0 "/>
+            8:00 a.m. -  5:00 p.m.
+          </p>
+          <p class="bussines-category">
+            {{ category_type.name }}
+          </p>
+        </div>
+      </div>
+      <div class="flex flex-col gap-[18px] text-sm leading-6 text-justify"> 
+        <p class="text-gray-20">{{ advertisement.business.description }}</p>
+      </div>
+      <MoleculesCharacteristics
+        :facebook="advertisement.business.facebook"
+        :whatsapp="advertisement.business.whatsapp"
+        :phone="advertisement.business.phone"
+        :email="advertisement.business.email"
+        :instagram="advertisement.business.instagram"
+        :website	="advertisement.business.webpage"
+        class="contact-info"
+      />
+    </section>
+    <section class="max-w-[1250px] mx-auto lg:px-5">
+      <h3 class="w-full border-b border-b-[#F5F5F5] text-[28px] font-medium py-2.5 mb-5">
+        Ubicación
+      </h3>
+      <div class="w-full pb-20">
+        <ClientOnly>
+          <iframe class="rounded-2xl w-full h-[240px]" :src="renderMap"></iframe>
+        </ClientOnly>
+      </div>
+    </section>
+  </main>
   <div class="p-16" v-if="pending">
     <OrganismSkeleton class="md:h-[560px] h-[360px] max-w-6xl mb-7" />
     <OrganismSkeleton class="w-40 h-8 mb-2" />
@@ -25,13 +65,17 @@
 </template>
 
 <script setup>
+const open = ref(true);
 const route = useRoute();
 const config = useRuntimeConfig();
+const categories = useCategories().categories;
+const category_type = ref(null);
+const renderMap = ref(null);
 
-const { data: property, pending, error} = await useLazyFetch(`advertisements/${route.query.property_id}`, {
+const { data: advertisement, pending, error} = await useLazyFetch(`advertisements/${route.query.property_id}`, {
   method: 'GET',
   baseURL: config.public.API,
-  transform:(_property) => _property.results,
+  transform:(_advertisement) => _advertisement.results,
   onResponse({response}){
     if(response.status === 400) {
       return navigateTo('/notFound')
@@ -39,9 +83,29 @@ const { data: property, pending, error} = await useLazyFetch(`advertisements/${r
   }
 });
 
-console.log(property)
+watchEffect(()=> {
+  if(advertisement !== null) {
+    category_type.value = categories.value.find(element => element.id === advertisement.value.business.business_category_id);
+    renderMap.value = `https://maps.google.com/maps?q=${advertisement.value.business.latitude},${advertisement.value.business.longitude}&hl=es;z%3D14&amp&output=embed`;
+  }
+})
 
 definePageMeta({
   middleware: ["not-found"]
 });
 </script>
+
+<style lang="postcss" scoped>
+.hour{
+  @apply flex bg-[#D3FFD5] text-[#4CAF50] relative items-center justify-center rounded-lg h-8 pl-8 pr-2 before:content-['Abierto:'] before:mr-1 whitespace-nowrap;
+  &.closed {
+    @apply bg-[#FFE4E3] text-[#FF2625] before:content-['Cerrado:'];
+  }
+}
+.bussines-category{
+  @apply text-primary-100 bg-[#F1F1F1] flex items-center justify-center text-center px-2 rounded-lg h-8;
+}
+.contact-info{
+  @apply lg:gap-x-16 gap-x-8 bg-[#FCFCFC] py-6 px-10 rounded-2xl w-full items-center justify-center my-16 !important
+  }
+</style>
